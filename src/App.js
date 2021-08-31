9280226faab35c3ae2b7136eab093fdb32795873
Akshay Dhaygude <adhaygude74@gmail.com>
2021-08-31 13:25:59 +0530

    add button

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import React from"react";
 import './App.css';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
    <>
             <h1> Calculator </h1>
             <hr />
+            <div>
+               <button>1 </button>
+               <button>2 </button>
+               <button> 3</button>
+               <button> 4</button>
+               <button> 5</button>
+               <button>6 </button>
+               <button>7 </button>
+               <button> 8</button>
+               <button> 9</button>
+               <button> 0</button>
+            </div>
    </>
    
   );
